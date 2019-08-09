45a74f8c98b06c996cfa080ec70a01a31486545f
Matheus Ramos <matheus.ramos@ifood.com.br>
2019-08-08 22:09:37 -0300

    add random quotes from galo cego

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { PureComponent } from "react";
+
+import galo from "./galo.png";
+
+class App extends PureComponent {
+  static quotes = [
+    "Não, é por causa de ser ator e ser acusado pelos pensamentos que se falam as perguntas erradas.",
+    "Largar mão de ser besta,se procurar de ser homi,criar vergonha cara,se tomar decumento, trabaiar.",
+    "Vida para o resto da sua vida...",
+    "Eu nunca te amo...\nEu te aludo.",
+    "Meu corpo é um corpo estadual, legal e bonito.",
+    "Se tem que se fazer, tem que se fazer com a própria pessoa que se merece.\nEu não se penso em fazer maldade com ninguém.",
+    "mas não tinha nada acontecido, se tivesse acontecido então eu não se mereço isso se tem que ser fazer tem que se fazer com a propria pessoa que merece isso, não com a vida inocente",
+    "é ué"
+  ];
+
+  state = {
+    quote: ""
+  };
+
+  getRandomQuote = () => {
+    const size = App.quotes.length;
+    const random = Math.random() * size;
+    const index = Math.floor(random) % size;
+
+    this.setState({
+      quote: App.quotes[index]
+    });
+  };
+
+  render() {
+    const { quote } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>{quote && `"${quote}"`}</p>
+          <a className="App-link" onClick={this.getRandomQuote}>
+            <img src={galo} className="App-logo" alt="logo" />
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
